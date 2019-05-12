bb8c86d9ff0ad1688efbdb3d535638396d294dbb
Felipeariza <42219613+Felipeariza@users.noreply.github.com>
2019-05-11 23:19:51 -0500

    Arreglo codigo

--- Fourier.py
+++ Fourier.py
@@ -1,42 +1,40 @@
 import numpy as np
+from scipy.fftpack import ifft, fft, fftfreq
 import matplotlib.pyplot as plt
-from scipy import interpolate
-
-
-# Se leen los datos de signal.dat 
-signal = np.genfromtxt('signal.dat',usecols=[0,2])
-
-# Se grafican los datos y se guarda en un pdf
-plt.plot(signal[:,0],signal[:,1],label='Signal')
-plt.title('Senal Signal')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend()
-plt.savefig('ArizaFelipe_signal.pdf')
+#Se cargan los datos signal.dat y sumaSignal.dat
+#Se utiliza unpack para separar los valores directamente
+S_signal,S_signal_t=np.genfromtxt('signalSuma.dat', unpack=True)
+signal,signal_t=np.genfromtxt('signal.dat', unpack=True)
+#Plot de los datos
+plt.figure()
+grafica,ejes=plt.subplots(2,1,1)
+ejes[0].plot(signal,signal_t,c='yellow')
+ejes[1].plot(S_signal,S_signal_t,c='red')
+ejes[0].set_xlabel('Amplitud')
+ejes[0].set_ylabel('Tiempo')
+ejes[1].set_xlabel('Amplitud Suma')
+ejes[1].set_ylabel('Tiempo')
+ejes[0].set_title('Datos Signal')
+ejes[1].set_title('Datos Signal Suma')
+plt.savefig('Datos_iniciales.pdf')
+#plt.show()
 plt.close()
-
-# Se hace la implementacion de fourier
-def fourier(datos):
-	fourier=[]
-	for n in range(0,len(datos)):
-		f=0.0
-		for k in range(0,len(datos)):
-			f+= (datos[k])*(np.cos(-2*np.pi*k*n/len(datos))+1j*np.sin(-2*np.pi*k*n/len(datos)))
-		fourier.append(f)
-	return fourier
-
-fts=fourier(signal[:,1])
-fts=np.asarray(fts) 
-
-ffts=np.fft.fftfreq(len(fts),0.00015)
-
-####Cuarto punto: Se hace la grafica de la transformada de Fourier y se guarda en un pdf
-plt.plot(abs(ffts),abs(fts),label='Signal',color='g')
-plt.xlabel('Frecuencia')
-plt.ylabel('Amplitud')
-plt.title('Fourier de signal')
-plt.legend()
-plt.xlim([0,1000])
-plt.savefig('ArizaFelipe_TF.pdf')
-plt.close()
-
+#Se define la implementacion propia de la transformada discreta de Fourier
+# Se hace la implementacion de fourier cabiando la forma del exponencial debido a que presento problemas corriendo con los el seno y coseno
+#def fourier(datos):
+#	fourier=[]
+#	for n in range(0,len(datos)):
+#		f=0.0
+#		for k in range(0,len(datos)):
+#			f+= (datos[k])*(np.cos(-2*np.pi*k*n/len(datos))+1j*np.sin(-2*np.pi*k*n/len(datos)))
+#		fourier.append(f)
+#	return fourier
+def transformada(datos):
+    fourier=[]
+    for i in range(len(datos)):
+        #Se inicializa la variable que guarda el valor de la transformada para cada punto y luego se agrega al arreglo
+        trans_f=0+0j
+        for l in range(len(datos)):
+            trans_f+=datos[l]*np.exp(-(2j*np.pi*i*l)/len(datos))
+        fourier.append(trans_f)
+    return np.array(fourier)
